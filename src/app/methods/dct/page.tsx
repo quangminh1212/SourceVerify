158baaf13dc00b47052bb5a21c9df8dcaec2d61a
Bach Minh Quang <0947556012abc@gmail.com>
2026-03-01 11:04:28 +0700

    Revert "refactor: replace 40 static method pages with single dynamic [slug] route"
    This reverts commit 5231b4635e4b1ba34d921567c11ab435fbdf9797.

---
+++ src/app/methods/dct/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import MethodDetail from "../_components/MethodDetail";
+import en from "./i18n/en.json";
+import vi from "./i18n/vi.json";
+import zh from "./i18n/zh.json";
+import ja from "./i18n/ja.json";
+import ko from "./i18n/ko.json";
+import es from "./i18n/es.json";
+
+const i18n = { en, vi, zh, ja, ko, es };
+
+export default function Page() {
+    return <MethodDetail methodId="dct" translations={i18n} />;
+}
