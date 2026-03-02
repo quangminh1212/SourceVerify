409e3db9d4cfea2c6575a2fc1aea331d7258ccb9
Bach Minh Quang <0947556012abc@gmail.com>
2026-03-02 11:05:10 +0700

    feat: add UI pages for 10 new text analysis methods with en/vi translations

---
+++ src/app/methods/text/word_frequency_rank/page.tsx
@@ -0,0 +1,10 @@
+﻿"use client";
+import MethodDetail from "../../_components/MethodDetail";
+import en from "./i18n/en.json";
+import vi from "./i18n/vi.json";
+
+const i18n = { en, vi };
+
+export default function Page() {
+    return <MethodDetail methodId="word_frequency_rank" translations={i18n} />;
+}
